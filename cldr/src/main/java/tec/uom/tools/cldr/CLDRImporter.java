b8dfc88999789cf33339f5ee1268baeb51866f34
Werner Keil <werner.keil@gmx.net>
2015-04-04 19:34:57 +0200

    3: Add Unicode CLDR support
    Task-Url:
    https://github.com/unitsofmeasurement/uom-tools/issues/issues/issue/3

---
+++ cldr/src/main/java/tec/uom/tools/cldr/CLDRImporter.java
@@ -0,0 +1,202 @@
+/**
+ * Unit-API - Units of Measurement API for Java
+ * Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.uom.tools.cldr;
+
+import io.airlift.airline.Arguments;
+import io.airlift.airline.Cli;
+import io.airlift.airline.Command;
+import io.airlift.airline.Help;
+import io.airlift.airline.Option;
+import io.airlift.airline.OptionType;
+import io.airlift.airline.Cli.CliBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.lang.model.SourceVersion;
+import javax.measure.format.ParserException;
+import javax.tools.Tool;
+
+/**
+ * @author Werner
+ * @version 0.2
+ */
+public class CLDRImporter implements Tool {
+	// TODO factor out, e.g. into uom-tools-common
+	static enum ErrorCode {
+		OK, Failure
+		// For now we'll use ordinal, but should change to code or Id (e.g. using IntIdentifiable)
+	}
+	
+	private static final Logger logger = Logger.getLogger(CLDRImporter.class.getName());
+	
+	static boolean isVerbose;
+	
+	// TODO this could be reusable, too, e.g. in uom-tools-common
+    static class ToolCommand implements Runnable {
+        @Option(type = OptionType.GLOBAL, name = "-v", description = "Verbose mode")
+        public boolean verbose;
+
+        public void run() {
+            System.out.println(getClass().getSimpleName());
+        }
+    }
+    
+    @Command(name = "display", description = "Display elements")
+    public static final class Display extends ToolCommand {
+        @Option(name = "-i", description = "Input file")
+        public String inFile;
+        
+        @Override
+        public void run() {
+        	if (inFile!=null && inFile.length()>0) {
+        		logger.info(getClass().getSimpleName() + " " + inFile);
+        	} else {
+        		logger.info(getClass().getSimpleName());
+        	}
+        	
+        	isVerbose = verbose;
+        	
+//        	if (verbose) {        	     	
+				if (inFile!=null && inFile.length()>0) {
+					try {
+						CLDRParser parser = new CLDRParser(true); // for display we currently verbose output
+						parser.load(inFile);
+					} catch (ParserException pe) {
+						logger.log(Level.WARNING, getClass().getSimpleName() + " error", pe);
+					} catch (IOException ioe) {
+						logger.log(Level.SEVERE, getClass().getSimpleName() + " loading error", ioe);
+					}
+				}
+//        	}
+        }
+    }
+    
+    @Command(name = "write", description = "Write to file")
+    public static final class Write extends ToolCommand {
+        @Option(name = "-i", description = "Input file")
+        public String inFile;
+
+        @Option(name = "-u", description = "Units output file (CSV)")
+        public String unitOutFile;
+        
+        @Option(name = "-q", description = "Quantities output file")
+        public String quantOutFile;
+        
+        @Option(name = "-s", description = "Sort entries")
+        public boolean sorted;
+        
+        @Arguments(description = "Quantities to write (Filter)")
+        public List<String> quantityFilter;
+        
+        @Override
+        public void run() {
+        	final StringBuilder message = new StringBuilder(getClass().getSimpleName());
+        	if ((unitOutFile!=null && unitOutFile.length()>0) || 
+        		(quantOutFile!=null && quantOutFile.length()>0)) {
+        		message.append(" to ");
+        		if (unitOutFile!=null && unitOutFile.length()>0) {
+        			message.append(unitOutFile);
+        			if (quantOutFile!=null && quantOutFile.length()>0) {
+        				message.append(", ");
+        				message.append(quantOutFile);
+        			}
+        		} else {
+        			if (quantOutFile!=null && quantOutFile.length()>0) message.append(quantOutFile);
+        		}
+        	}
+        	logger.info(message.toString());
+        	isVerbose = verbose;
+        	
+//        	if (quantities != null) {
+//		        for (String q : quantities) {
+//		        	System.out.println(q);
+//		        }
+//        	}
+        	
+			if ((inFile!=null && inFile.length()>0) && 
+			   ((unitOutFile!=null && unitOutFile.length()>0) ||
+			    (quantOutFile!=null && quantOutFile.length()>0))) {
+				try {
+					CLDRParser parser = new CLDRParser(verbose);
+					parser.load(inFile, unitOutFile, quantOutFile);
+				} catch (ParserException pe) {
+					logger.log(Level.WARNING, getClass().getSimpleName() + " error", pe);
+				} catch (IOException ioe) {
+					logger.log(Level.SEVERE, getClass().getSimpleName() + " loading error", ioe);
+				}			
+			}
+        }
+    }
+	
+	/* (non-Javadoc)
+	 * @see javax.tools.Tool#run(java.io.InputStream, java.io.OutputStream, java.io.OutputStream, java.lang.String[])
+	 */
+	@Override
+	public int run(InputStream in, OutputStream out, OutputStream err,
+			String... arguments) {
+		try {
+			@SuppressWarnings("unchecked")
+			CliBuilder<Runnable> builder = Cli.<Runnable>builder(getClass().getSimpleName())
+		                .withDescription("Unicode CLDR Importer Tool")
+		                .withDefaultCommand(Help.class)
+		                .withCommands(Help.class, Display.class)
+		                .withCommands(Help.class, Write.class)
+		                ;
+		        Cli<Runnable> toolParser = builder.build();
+		        toolParser.parse(arguments).run();	
+				
+				return ErrorCode.OK.ordinal();
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+			return ErrorCode.Failure.ordinal();
+		}
+	}
+
+	@Override
+	public final Set<SourceVersion> getSourceVersions() {
+		return Collections.unmodifiableSet(new HashSet<SourceVersion>(Arrays.asList(
+				new SourceVersion[]{SourceVersion.RELEASE_5, SourceVersion.RELEASE_6, 
+						SourceVersion.RELEASE_7 } )));
+	}
+
+	public static void main(String[] args) throws Exception {
+		Tool importer = new CLDRImporter();
+		int errorCode = importer.run(System.in, System.out, System.err, args);
+		if (errorCode == ErrorCode.OK.ordinal()) {
+			if (isVerbose) System.out.println("Success.");
+		}
+	}
+}
---
+++ cldr/src/main/java/tec/uom/tools/cldr/CLDRParser.java
@@ -0,0 +1,153 @@
+/**
+ * Unit-API - Units of Measurement API for Java
+ * Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.uom.tools.cldr;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.measure.format.Parser;
+import javax.measure.format.ParserException;
+
+import tec.units.ri.spi.SIPrefix;
+
+import org.apache.commons.io.IOUtils;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+/**
+ * @author Werner
+ * @version 0.2
+ */
+final class CLDRParser implements Parser<Object, String> {
+
+	/**
+	 * If called stand-alone
+	 * 
+	 * @param args
+	 * @throws Exception
+	 * @deprecated remove after test/debugging
+	 */
+	public static void main(String[] args) throws Exception {
+		CLDRParser parsing = new CLDRParser(true);
+		parsing.load("/root/units.json");
+	}
+
+	private static final Logger logger = Logger.getLogger(CLDRParser.class
+			.getName());
+	
+	private final boolean verbose;
+	private final Set<String> unitSet = new HashSet<>();
+	private final Set<String> quantitySet = new HashSet<>();
+	
+	CLDRParser(boolean v) {
+		this.verbose = v;
+	}
+	
+	void load(String... files) throws IOException, ParserException {
+		if (files != null && files.length > 0) {
+			InputStream is = CLDRParser.class.getResourceAsStream(files[0]);
+			String jsonTxt = IOUtils.toString(is);
+
+			final JSONObject json = (JSONObject) JSONSerializer.toJSON(jsonTxt);
+			// Set entries = json.entrySet();
+			//
+			// for (Object o : entries) {
+			// System.out.println(o);
+			// }
+			final JSONObject main = json.getJSONObject("main");
+			final JSONObject root = main.getJSONObject("root");
+//			JSONObject id = root.getJSONObject("identity");
+			final JSONObject units = root.getJSONObject("units");
+			// System.out.println(units);
+			final JSONObject longUnits = units.getJSONObject("long");
+			// System.out.println(longUnits);
+			@SuppressWarnings("rawtypes")
+			Set entries = longUnits.entrySet();
+
+			for (Object o : entries) {
+				handle(parse(o));
+			}
+
+//			for (SIPrefix prefix : SIPrefix.values()) {
+//				System.out.println(prefix);
+//			}
+			if (unitSet.size()>0 && files.length>1) {
+				writeToFile(unitSet, files[1]);
+			}
+			if (quantitySet.size()>0 && files.length>2) {
+				writeToFile(quantitySet, files[2]);
+			}
+		}
+	}
+
+	@Override
+	public String parse(Object input) throws ParserException {
+		return String.valueOf(input);
+	}
+	
+	// Internals
+	
+	private void handle(String st) {
+		String[] subStrings = st.split("=");
+		if (subStrings != null && subStrings.length > 0) {
+			handleUnit(subStrings[0]);
+		}
+	}
+
+	private void handleUnit(String unitEntry) {
+		if (unitEntry.contains("-")) {
+			int quantCutOff = unitEntry.indexOf("-");
+			String quantity = unitEntry.substring(0, quantCutOff);
+			quantitySet.add(quantity);
+			String unit = unitEntry.substring(quantCutOff+1);
+			unitSet.add(quantity + ";" + unit);
+			if (verbose) System.out.println(quantity + ": " + unit);
+		} else {
+			logger.warning(String.format("'%s' has no quantity, ignoring.",
+					unitEntry));
+		}
+	}
+	
+    private void writeToFile(final Set<String> strings, final String fileName) {
+    	if (fileName!=null && fileName.length()>0) {
+	    	try (FileWriter fw = new FileWriter(fileName);
+	    		 BufferedWriter bw = new BufferedWriter(fw)) {        	        	
+	    		for (String s : strings) {
+	    			bw.write(s);
+	    			bw.newLine();
+	    		}        	 
+	    	} catch (Exception e) {
+	    		logger.warning(e.getMessage());
+	    	}
+    	}
+    }
+}
