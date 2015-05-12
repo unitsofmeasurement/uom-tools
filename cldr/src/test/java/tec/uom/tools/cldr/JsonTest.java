76efdddd8fc3450b8bfa74e711d5d8561d8ff98f
wick3dshadow <anakarparida@gmail.com>
2015-05-13 00:56:10 +0530

    JSON-P implementation instead of JSON

--- cldr/src/test/java/tec/uom/tools/cldr/JsonTest.java
+++ cldr/src/test/java/tec/uom/tools/cldr/JsonTest.java
@@ -2,10 +2,11 @@ package tec.uom.tools.cldr;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
+import java.io.StringReader;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonReader;
 
 import org.junit.Test;
 
@@ -13,15 +14,27 @@ public class JsonTest {
 
 	@Test
 	public void test() {
-		String json = "{name=\"json\",bool:true,int:1,double:2.2,func:function(a){ return a; },array:[1,2]}";  
-		JSONObject jsonObject = JSONObject.fromObject( json );  
-		Object bean = JSONObject.toBean( jsonObject );  
+		//String json = "{name=\"json\",bool:true,int:1,double:2.2,func:function(a){ return a; },array:[1,2]}";
+		String json = "[1, 2]";
+		//JsonArrayBuilder aBuilder = Json.createArrayBuilder();
+		//aBuilder.add("name");
+		//JsonArray array = aBuilder.build();
+		 JsonReader jsonReader = Json.createReader(new StringReader(json));
+		 JsonArray jsonArr = jsonReader.readArray();		 
+		 //JsonObject jsonObj = jsonReader.readObject();
+		 jsonReader.close();
+		//JSONObject jsonObject = new JsonObjectBuilder()..  .fromObject( json );  
+		//Object bean = JSONObject.toBean( jsonObject );
+		 assertFalse(jsonArr.isEmpty());
+		 assertEquals(2, jsonArr.size());
+		
 //		assertEquals( jsonObject.get( "name" ), PropertyUtils.getProperty( bean, "name" ) );  
 //		assertEquals( jsonObject.get( "bool" ), PropertyUtils.getProperty( bean, "bool" ) );  
 //		assertEquals( jsonObject.get( "int" ), PropertyUtils.getProperty( bean, "int" ) );  
 //		assertEquals( jsonObject.get( "double" ), PropertyUtils.getProperty( bean, "double" ) );  
-//		assertEquals( jsonObject.get( "func" ), PropertyUtils.getProperty( bean, "func" ) );  
-		List expected = JSONArray.toList( jsonObject.getJSONArray( "array" ) );  
+//		assertEquals( jsonObject.get( "func" ), PropertyUtils.getProperty( bean, "func" ) );
+		//List expected = array.getValuesAs(ValueType.class);
+		//List expected = JSONArray.toList( jsonObject.getJSONArray( "array" ) );  
 //		Assertions.assertListEquals( expected, (List) PropertyUtils.getProperty( bean, "array" ) );  
 	}
 
